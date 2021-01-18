057b02e969b9b6f2ac51fde6a9416bf11f170ca1
Tyler Casper <casperw@colorado.edu>
2021-01-18 15:00:19 -0600

    More tidying up of env-testing.py to better understand the lunar lander code.

--- LunarLander_v2/env-testing.py
+++ LunarLander_v2/env-testing.py
@@ -16,6 +16,23 @@ import numpy as np
 ##   Landing outside the landing pad is possible. Fuel is infinite, so an agent can learn to fly and then land
 ## on its first attempt. Please see the source code for details.
 ##
+##  OBSERVATION/STATE: 8 floats in a flat array
+##          pos.x
+##          pos.y 
+##          vel.x
+##          vel.y
+##          self.lander.angle,
+##          self.lander.angularVelocity
+##          1.0 if self.left_leg.ground_contact else 0.0,
+##          1.0 if self.right_leg.ground_contact else 0.0
+##
+##  DISCRETE ACTION: one integer
+##    - 0 for no action, 1 for left engine, 2 for main engine, 3 for right engine
+##
+##  CONTINUOUS ACTION: two floats [ main engine[-1:(0.5:1)], left-right engines[(1:-0.5):(0.5:1)] ].
+##    - Main engine: -1..0 off, 0..+1 throttle from 50% to 100% power. Engine can't work with less than 50% power.
+##    - Left-right:  -1.0..-0.5 fire left engine, +0.5..+1.0 fire right engine, -0.5..0.5 off
+##
 ## NOTES: - less fuel spent is better, about -30 for heuristic landing
 ##        -
 ##        -  
@@ -26,17 +43,13 @@ env = gym.make("LunarLander-v2")
 
 observation = env.reset() # vector (or matrix) of values...ANY INFO WE HAVE == observation
 
-print(observation)
-print(env.action_space)
-print(env.observation_space)
-
 done = False
 
 i = 0
 
 while not done: # loop environment 
 
-    observation, reward, done, info = env.step(env.action_space.sample()) # env.action_space.sample() is where our agent goes. for now, it is random sampling as a placeholder
+    observation, reward, done, info = env.step(0) # env.action_space.sample() is where our agent goes. for now, it is random sampling as a placeholder
     action = env.action_space.sample()
 
     i += 1
@@ -49,3 +62,8 @@ while not done: # loop environment
     env.render()
 
 env.close()
+
+print(observation)
+print(env.action_space)
+print(env.observation_space)
+print(env.action_space.sample())
